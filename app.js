cf5c73615c0c4fbf7487a6daf832f45ef432e81f
laavis <sara.suviranta@gmail.com>
2018-11-21 13:22:09 +0200

    do the register form

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const app = express();
 const port = 8000;
 
 app.set('trust proxy', 1);
-const sslKey = fs.readFileSync('/etc/pki/tls/private/ca.key');
-const sslCert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
+//const sslKey = fs.readFileSync('/etc/pki/tls/private/ca.key');
+//const sslCert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
 
-const options = {
+/* const options = {
   key: sslKey,
   cert: sslCert,
-};
+}; */
 
 // Handlebars middleware
 app.engine(
@@ -30,7 +30,7 @@ app.set('views', __dirname + '/src/views');
 app.set('view engine', 'hbs');
 
 // Static folder
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use('/static', express.static(path.join(__dirname, '/dist')));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -50,6 +50,6 @@ app.get('/register', (req, res) => {
 
 app.listen(port);
 
-https.createServer(options, app).listen(3000, () => {
+/* https.createServer(options, app).listen(3000, () => {
   console.log(`Server started on port ${port}...`);
-});
+}); */
