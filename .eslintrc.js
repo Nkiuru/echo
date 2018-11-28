d69359460c671eaafe7e4bb1b7c445fab4685a7e
Nkiuru <niklas.kiuru1@gmail.com>
2018-11-28 13:32:23 +0200

    Fix eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,6 @@ module.exports = {
   rules: {
     'max-len': ["error", { "code": 120 }],
     "indent": ["error", 2],
-    "object-curly-spacing": "never",
+    "object-curly-spacing": [1, "always"],
   }
 };
