33e9cf0782050955c2d06a187b02a78e1d335e7c
Nkiuru <niklas.kiuru1@gmail.com>
2018-12-09 20:44:35 +0200

    Added comments for user

--- src/user/init.js
+++ src/user/init.js
@@ -7,8 +7,16 @@ const upload = multer({ dest: (path.join(__dirname, '../../dist/uploads')) });
 const db = require('../modules/database');
 const uploadJs = require('../modules/upload');
 
+/*
+* List of API endpoints for all user related requests
+*/
 const initUser = (app) => {
   app.get('/', renderWelcome);
+
+  /*
+  * Endpoint for logining in
+  * Returns success: true if credentials match
+  */
   app.post('/login', (req, res, next) => {
     passport.authenticate('local', (err, user, info) => {
       if (err) {
@@ -77,16 +85,28 @@ const testAuth = (req, res) => {
   res.render('users');
 };
 
+/*
+* Creates a new user
+* Requires password, password2, username, displayName, countryId, city, and email
+* Returns success: true if account was created successfully
+*/
 const addUser = (req, res, next) => {
   users.createUser(req, res);
 };
-
+/*
+* Renders the user profile page
+*/
 const getUser = (req, res) => {
   res.render('profile', {
     // posts: results.map(x => ({ ...x, username: req.user.username })),
   });
 };
-
+/*
+* Gets all posts made by the user
+* Requires the username of the user
+* User must be authenticated
+* Returns all posts with relevant data
+*/
 const getUserPosts = (req, res) => {
   if (req.params.username) {
     users.getUserWithUsername(req.params.username).then((user) => {
@@ -104,7 +124,11 @@ const getUserPosts = (req, res) => {
     });
   }
 };
-
+/*
+* Gets the user
+* Requires the username of the user
+* Returns user with all relevant data
+*/
 const getUserData = (req, res) => {
   if (req.params.hasOwnProperty('username')) {
     users.getUserWithUsername(req.params.username).then((user) => {
@@ -119,6 +143,11 @@ const getUserData = (req, res) => {
   }
 };
 
+/*
+* Gets the user data of the requester
+* User must be authenticated
+* Returns all relevant user data except password
+*/
 const getOwnData = (req, res) => {
   if (req.user) {
     users.getOwnData(req, res);
@@ -128,10 +157,21 @@ const getOwnData = (req, res) => {
   }
 };
 
+/*
+* Changes the requester's password
+* User must be authenticated
+* Requires oldPassword and newPassword
+* Returns a message if successfully updated
+*/
 const updatePassword = (req, res) => {
   users.changePassword(req, res);
 };
 
+/*
+* Logs out the requester
+* Clears the cookie
+* Returns success: true if successful
+*/
 const logoutUser = (req, res) => {
   res.clearCookie('connect.sid');
   res.json({ success: true });
@@ -139,6 +179,12 @@ const logoutUser = (req, res) => {
   res.end();
 };
 
+/*
+* Updates the user's data
+* Requires displayName and email
+* Bio or city if not given will be set to null
+* Returns all relevant user data
+*/
 const uploadUserData = (req, res, next) => {
   uploadJs.createUpload(req, res, next).then((uploadId) => {
     const data = [
@@ -155,6 +201,12 @@ const uploadUserData = (req, res, next) => {
   }).catch((err) => console.log(err));
 };
 
+/*
+* Updates the user's data
+* Requires displayName, email and profile image
+* Bio or city if not given will be set to null
+* Returns all relevant user data
+*/
 const uploadUserDataNoFile = (req, res, next) => {
   console.log(req.body);
   const data = [
