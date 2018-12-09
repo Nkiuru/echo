ea0b5e484992c87d7433830257e7be4d66f99bf3
Nkiuru <niklas.kiuru1@gmail.com>
2018-12-09 20:05:19 +0200

    Add comments for authentication

--- src/authenticate/init.js
+++ src/authenticate/init.js
@@ -6,17 +6,21 @@ const LocalStrategy = require('passport-local').Strategy;
 const db = require('../modules/database');
 const authenticationMiddleware = require('./middleware');
 
-// configure passport.js to use the local strategy
+/*
+* configure passport.js to use the local strategy
+* Get the user with username and check if the password hashes match using bcrypt.
+* Returns an error message if the credentials do not match
+*/
 const initPassport = () => {
   passport.use(new LocalStrategy(
-    {usernameField: 'username'},
+    { usernameField: 'username' },
     (username, password, done) => {
       db.getUserWPassword(username)
         .then((result) => {
           if (result.length > 0) {
             const user = result[0];
             if (!user) {
-              return done(null, false, {message: 'Invalid credentials.'});
+              return done(null, false, { message: 'Invalid credentials.' });
             }
             bcrypt.compare(password, user.password)
               .then((result) => {
@@ -24,10 +28,10 @@ const initPassport = () => {
                   console.log(username + ' logged in');
                   return done(null, user.userId);
                 }
-                return done(null, false, {message: 'Invalid credentials.'});
+                return done(null, false, { message: 'Invalid credentials.' });
               });
           } else {
-            return done(null, false, {message: 'Invalid credentials.'});
+            return done(null, false, { message: 'Invalid credentials.' });
           }
         })
         .catch((error) => done(error));
@@ -35,10 +39,16 @@ const initPassport = () => {
   passport.authenticationMiddleware = authenticationMiddleware;
 };
 
+/*
+* Serialize user using the userId.
+ */
 passport.serializeUser((userId, done) => {
   done(null, userId);
 });
 
+/*
+* Deserialize user and attach the user to each request with all user data except password
+ */
 passport.deserializeUser((id, done) => {
   db.getUserByIdWEmail(id)
     .then((res) => done(null, res[0]))
--- src/post/init.js
+++ src/post/init.js
@@ -1,3 +1,4 @@
+'use strict';
 const passport = require('passport');
 const multer = require('multer');
 const path = require('path');
